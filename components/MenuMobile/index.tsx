ea247035f0ae3f10a23e3e825c67d18e1c1dcd4b
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2023-02-01 13:11:52 -0300

    chore: removed children links

--- components/MenuMobile/index.tsx
+++ components/MenuMobile/index.tsx
@@ -21,38 +21,35 @@ const MenuMobile = ({ isOpen, toggle }: Props) => {
 
   return (
     <MenuMobileContainer isOpen={isOpen} className="menuMobile">
-      <MenuMobileContent id="menu-mobile" aria-expanded={isOpen} >
-        <Link href="/">
-          <a
-            style={{
-              color: pathname === "/" ? "#43C59E" : "#DFE1E5",
-            }}
-            onClick={toggle}
-          >
-            <House size={32} weight="fill" />
-            {t("home")}
-          </a>
+      <MenuMobileContent id="menu-mobile" aria-expanded={isOpen}>
+        <Link
+          href="/"
+          style={{
+            color: pathname === "/" ? "#43C59E" : "#DFE1E5",
+          }}
+          onClick={toggle}
+        >
+          <House size={32} weight="fill" />
+          {t("home")}
         </Link>
-        <Link href="/projetos">
-          <a
-            style={{
-              color: pathname === "/projetos" ? "#43C59E" : "#DFE1E5",
-            }}
-            onClick={toggle}
-          >
-            <Browsers size={32} weight="fill" />
-            {t("projects")}
-          </a>
+        <Link
+          href="/projetos"
+          style={{
+            color: pathname === "/projetos" ? "#43C59E" : "#DFE1E5",
+          }}
+          onClick={toggle}
+        >
+          <Browsers size={32} weight="fill" />
+          {t("projects")}
         </Link>
 
-        <Link href="/contato">
-          <a
-            style={{ color: pathname === "/contato" ? "#43C59E" : "#DFE1E5" }}
-            onClick={toggle}
-          >
-            <EnvelopeSimple size={32} weight="fill" />
-            {t("contact")}
-          </a>
+        <Link
+          href="/contato"
+          style={{ color: pathname === "/contato" ? "#43C59E" : "#DFE1E5" }}
+          onClick={toggle}
+        >
+          <EnvelopeSimple size={32} weight="fill" />
+          {t("contact")}
         </Link>
       </MenuMobileContent>
       <MenuButton onClick={toggle} aria-controls="menu-mobile">
