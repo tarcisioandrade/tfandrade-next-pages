ee943880c6dcc2fee191d84845a770ffd80a2ee2
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2023-02-07 15:23:58 -0300

    fix: error env

--- sanity.ts
+++ sanity.ts
@@ -2,10 +2,10 @@ import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: "production",
+  projectId: "9kjy77bb",
   apiVersion: "2021-03-25",
-  useCdn: process.env.NODE_ENV === "production",
+  useCdn: true,
 };
 
 export const sanityClient = createClient(config);
